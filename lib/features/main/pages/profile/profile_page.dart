834b9ac18adac4322280d9b41b877d3f583b6b1d
fnt4sia <fitraramadhan668@gmail.com>
2024-04-13 23:46:29 +0700

    test : unused import

--- lib/features/main/pages/profile/profile_page.dart
+++ lib/features/main/pages/profile/profile_page.dart
@@ -1,7 +1,4 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
-import './profile_model.dart';
 
 class ProfilePage extends StatefulWidget {
   const ProfilePage({super.key});
