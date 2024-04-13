e69c652a30f8629447472b5cf5852a58eeacb2c0
fnt4sia <fitraramadhan668@gmail.com>
2024-04-13 23:53:41 +0700

    styles : addding color file

---
+++ lib/shared/themes/color.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+const owlBase = Color(0xffEEF9FD);
+const owlOrange = Color(0xffFBA834);
+const owlDarkBlue = Color(0xff333A73);
+const owlMidBlue = Color(0xff387ADF);
+const owlLightBlue = Color(0xff50C4ED);
