ccb787a0dc75b23c503c3bd36871cc375524195e
fnt4sia <fitraramadhan668@gmail.com>
2024-05-05 21:17:17 +0700

    feat : fetch api quiz

---
+++ lib/features/main/pages/quiz/quiz_model.dart
@@ -0,0 +1,37 @@
+import 'dart:convert';
+
+import 'package:http/http.dart' as http;
+
+class Quiz {
+  final String questionQuiz;
+  final List listAnswer;
+  final int correctAnswer;
+
+  static List<Quiz> listQuiz = [];
+
+  Quiz({
+    required this.questionQuiz,
+    required this.listAnswer,
+    required this.correctAnswer,
+  });
+
+  static getQuiz(int topicId) async {
+    listQuiz = [];
+
+    final response = await http.get(
+      Uri.parse("https://nodejsdeployowl.et.r.appspot.com/getQuiz/51"),
+    );
+
+    final bodyResponse = await jsonDecode(response.body)['data'];
+
+    bodyResponse.map((dataQuiz) {
+      listQuiz.add(
+        Quiz(
+          questionQuiz: dataQuiz['question'],
+          listAnswer: dataQuiz['multiple_choice'],
+          correctAnswer: dataQuiz['answer'],
+        ),
+      );
+    }).toList();
+  }
+}
